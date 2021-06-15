e66c71eaebd1951312391972c24536bacca87cfb
SiyeyDev <siyeydev@gmail.com>
2021-06-15 10:39:37 -0500

    add lobby status

---
+++ Mobile_Multiplayer/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    [Header("Connection Status")]
+    public Text connectionStatusText;
+
+    [Header("Login UI Panel")]
+    public InputField playerNameInput;
+
+    #region Unity Regions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        connectionStatusText.text = "Connection Status: " + PhotonNetwork.NetworkClientState;   
+    }
+
+    #endregion
+
+    #region UI Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.Log("Playername is invalid");
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to photon server");
+    }
+
+
+    #endregion
+
+}
